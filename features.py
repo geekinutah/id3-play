eb9997bdb6eb6904a10551d959fe5cd1cf566de9
Mike Wilson <geekinutah@gmail.com>
2018-03-16 12:06:59 -0600

    Added features module and data sharing the same last name

---
+++ features.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Use this module to define features.
+"""
+
+def first_longer_than_last(name):
+    pass
+
+def has_middle_name(name):
+    pass
+
+def first_and_last_end_same_letter(name):
+    pass
+
+def first_alphabetically_before_last(name):
+    pass
+
+def second_letter_of_first_name_vowel(name):
+    pass
+
+def name_sum_is_even(name):
+    pass
+
+def first_name_begins_with_vowel(name):
+    pass
+
+def last_name_begins_with_vowel(name):
+    pass
+
+def vowels_greater_than_number(name, num_of_vowels):
+    pass
+
+
