36368e1d90993b9b27b65671375f9500655e37b1
Mike Wilson <geekinutah@gmail.com>
2018-03-18 00:04:52 -0600

    Some implementation of get_tree

--- id3_tree.py
+++ id3_tree.py
@@ -3,11 +3,25 @@ from treelib import Node, Tree
 import math
 import features
 
-def get_tree(data):
+def reduce_entropy(tree, features, data):
+    #Classify evreything using the current tree, if everything is labeled, return this tree
+    pass
+
+def get_tree(data, features):
     '''
        Take data, get a decision tree based on the id3 algorithm.
     '''
-    pass
+    feature_entropy = {}
+    # First get entropy for all features:
+    for f in features:
+        x = get_entropy(f, data)
+        feature_entropy[f] = x
+
+    # Now, choose the highest entropy, split on that node
+    ks = sorted(feature_entropy, key=lambda i: int(feature_entropy[i]))
+    tree = id3_tree()
+    tree.create_node(feature_entropy[ks[-1]])
+    reduce_entropy(tree, ks[:-1], data)
 
 def get_entropy(feature, data):
     yes = 0
@@ -21,10 +35,9 @@ def get_entropy(feature, data):
 
     avg_yes = float(yes) / (len(data))
     avg_no = float(no) / (len(data))
+
     return abs( (avg_yes * math.log(avg_yes,2) ) - (avg_no * math.log(avg_no,2)) )
 
-def  get_information_gain():
-    pass
 
 class id3_tree(Tree):
     '''
